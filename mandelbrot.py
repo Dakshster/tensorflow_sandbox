f0620e2e44d2ef5770ac6cdf8675b5dac1ea07c5
tsuda7 <tsuda.h.aa@gmail.com>
2016-01-17 16:53:08 +0900

    Add the original source code

--- mandelbrot.py
+++ mandelbrot.py
@@ -20,19 +20,22 @@ def DisplayFractal(a, fmt='jpeg'):
 # main
 if __name__ == "__main__":
   sess = tf.InteractiveSession()
+
+  # 描画する対象グリッドを生成
   Y, X = np.mgrid[-1.3:1.3:0.005, -2:1:0.005]
   Z = X+1j*Y
 
   xs = tf.constant(Z.astype("complex64"))
   zs = tf.Variable(xs)
+  # xs と同じ型の零行列を生成
   ns = tf.Variable(tf.zeros_like(xs, "float32"))
 
   tf.initialize_all_variables().run()
 
-  # Compute the new values of z: z^2 + x
+  # z = z^2 + x を新しく計算
   zs_ = zs*zs + xs
 
-  # Have we diverged with this new value?
+  # 発散しないかチェック（マンデルブロー集合の定義）
   not_diverged = tf.complex_abs(zs_) < 4
 
   # Operation to update the zs and the iteration count.
@@ -43,9 +46,12 @@ if __name__ == "__main__":
   #
   step = tf.group(
     zs.assign(zs_),
+    # boolean を float にキャスト(true -> 1, false -> 0)
+    # ns が各グリッドに置ける数列のカウントになっている（発散条件を満たす場合にのみ加算）
     ns.assign_add(tf.cast(not_diverged, "float32"))
-    )
+  )
 
+  # 数列を第200位まで計算
   for i in range(200):
     step.run()
 
